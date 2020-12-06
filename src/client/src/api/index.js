fd9e33b3da39826913f016ba651aa76aa42baaf9
Luong NGUYEN <luongnv@macbook-pro.home>
2020-12-06 21:43:10 +0100

    Update for dev env first

--- src/client/src/api/index.js
+++ src/client/src/api/index.js
@@ -1,6 +1,7 @@
 // read and pass the environment variables into reactjs application
-// const URL = `http://localhost:31057`;
-const URL = "";
+const URL = `http://localhost:31057`;
+// Comment the above line and uncomment the bellow one for production
+// const URL = "";
 
 // MODELS
 export const sendRequestAllROMs = async () => {
